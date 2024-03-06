aca80b7784cc71153816b0646fbc4586df78caef
alexandetlz <117372173+alexandetlz@users.noreply.github.com>
2024-03-06 16:32:12 +0700

    Create Max min.kt

---
+++ collections/Max min.kt
@@ -0,0 +1,12 @@
+// Return a customer who has placed the maximum amount of orders
+fun Shop.getCustomerWithMaxOrders(): Customer? =
+        customers.maxByOrNull { it.orders.size }
+
+// Return the most expensive product that has been ordered by the given customer
+fun getMostExpensiveProductBy(customer: Customer): Product? =
+        customer.orders
+                .flatMap(Order::products)
+                .maxByOrNull(Product::price)
+//mereturnkan value terbesar (max) atau terkecil (min) atau null dengan tambahan orNull bila tidak ditemukan
+
+                
