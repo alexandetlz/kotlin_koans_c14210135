7ac8dc3a9a83bb3031e35c77be7f04c06b4d3e83
alexandetlz <117372173+alexandetlz@users.noreply.github.com>
2024-03-06 16:10:18 +0700

    Create invoke.kt

---
+++ conventions/invoke.kt
@@ -0,0 +1,13 @@
+class Invokable {
+    var numberOfInvocations: Int = 0
+        private set
+
+    operator fun invoke(): Invokable {
+        numberOfInvocations++
+        return this
+    }
+}
+
+fun invokeTwice(invokable: Invokable) = invokable()()
+
+//kelas yang dibuat dapat dipanggil seperti sebuah function
